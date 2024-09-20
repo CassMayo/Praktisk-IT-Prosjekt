63283c974698bb9b3ea216409e2b5eb5a9539681
CassMayo <124171977+CassMayo@users.noreply.github.com>
2024-09-20 15:49:09 +0200

    CSS FILE

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d86d28f3a8cd6b9f92680f90e8bc9b4bbe895773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2775d6e8bc0f6d19eefc658d19cf2b87fd2c1b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
