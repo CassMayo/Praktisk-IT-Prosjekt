f9b988ce1693787b193d5b158d7c7911611bcbb2
CassMayo <124171977+CassMayo@users.noreply.github.com>
2024-09-24 13:09:24 +0200

    Change Context

--- api/Models/SenderDbContext.cs
+++ api/Models/SenderDbContext.cs
@@ -6,6 +6,7 @@ namespace api.Models
     {
         public SenderDbContext(DbContextOptions<SenderDbContext> options) : base(options)
         {
+            Database.EnsureDeleted();
             Database.EnsureCreated();
         }
 
