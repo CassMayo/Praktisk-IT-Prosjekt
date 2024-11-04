6afb07bb393959071bd0b958cd74b697c92b68d9
Nora Nicolaisen <nora.nicolaisen@live.no>
2024-11-04 15:37:22 +0100

    fixing navbar

--- swopp-client/src/Components/Navigation/NavBar.jsx
+++ swopp-client/src/Components/Navigation/NavBar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './NavBar.css'; // We will add styles here later
+import './NavBar.css'; 
 
 const NavBar = () => {
   return (
-    <nav className="bottom-nav">
-      <Link to="/home" className="nav-link">Home</Link> {/* Home Tab */}
-      <Link to="/UserPage" className="nav-link">User</Link> {/* User Tab */}
+    <nav className="nav-bar">
+      <div className="nav-container">
+        <Link to="/home" className="nav-link">Home</Link> {/* Home Tab */}
+        <Link to="/UserPage" className="nav-link">User</Link> {/* User Tab */}
+      </div>
     </nav>
   );
 };
